228de0238b19ecc969f82584bf5737df471826c6
Carlos Contreras <kradvenko@outlook.com>
2018-05-27 12:28:25 -0600

    Más cambios

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-    <link rel="stylesheet" type="text/css" href="css/Mapa.css" />
+    <link rel="stylesheet" type="text/css" href="css/pdv.css" />
     <script src="js/jquery-3.3.1.js"></script>
     <script src="js/bootstrap.js"></script>
-    <script src="js/zoo.js"></script>
+    <script src="js/pdv.js"></script>
 
     <title>Punto de Venta</title>
     <asp:ContentPlaceHolder ID="head" runat="server">
--- php/menu.php
+++ php/menu.php
@@ -23,10 +23,10 @@
                         <a class="nav-link mainMenuElement" href="menu.php">Menú principal</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mainMenuElement" href="localidades.php">Control de localidades</a>
+                        <a class="nav-link mainMenuElement" href="nuevaventa.php">Nueva venta</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mainMenuElement" href="instituciones.php">Control de instituciones</a>
+                        <a class="nav-link mainMenuElement" href="instituciones.php"></a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle mainMenuElement" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
